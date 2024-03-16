f6afbba117d700b6e02232af692f2b4214b539c3
hueralin <1612415787@qq.com>
2024-03-16 16:40:45 +0800

    chore: 贪心

--- src/leetcode/P455.java
+++ src/leetcode/P455.java
@@ -3,47 +3,64 @@ package leetcode;
 import java.util.Arrays;
 
 public class P455 {
-    // 遍历孩子，孩子能吃就吃
+    // g: 孩子胃口, s: 饼干尺寸
+
+    // 从小到大排序
     public int findContentChildren(int[] g, int[] s) {
+        // 从小到大排序
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int i = g.length - 1;
-        int j = s.length - 1;
-        int res = 0;
-
-        // 同时从后开始遍历
-        while (i >= 0 && j >= 0) {
+        // 用于遍历孩子胃口
+        int i = 0;
+        // 用于遍历饼干尺寸
+        int j = 0;
+        int count = 0;
+        // 遍历孩子的胃口和饼干尺寸
+        while (i < g.length && j < s.length) {
+            // 如果这块饼干的尺寸满足孩子的胃口，则分给他
             if (s[j] >= g[i]) {
-                // 如果当前饼干的 size 满足小孩的胃口，则把这个饼干给这个小孩
-                res++;
-                // 尝试下一个饼干
-                j--;
+                // 遍历下一个孩子
+                i++;
+                // 遍历下一块饼干
+                j++;
+                // 满足数 +1
+                count++;
+            } else {
+                // 不满足孩子胃口，则孩子等一等，遍历下一个饼干
+                j++;
             }
-            // 不管当前饼干满不满足这个小孩的胃口，接下来都要尝试下一个小孩
-            i--;
         }
 
-        return res;
+        return count;
     }
 
-    // 遍历饼干，饼干能给就给
     public int findContentChildren2(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
+        // 从大到小排序（MD，Java 不能这么写，真拉，JS 都可以）
+        Arrays.sort(g, (a, b) -> b - a);
+        Arrays.sort(s, (a, b) -> b - a);
 
+        // 用于遍历孩子胃口
         int i = 0;
+        // 用于遍历饼干尺寸
         int j = 0;
-        int res = 0;
-
+        int count = 0;
+        // 遍历孩子的胃口和饼干尺寸
         while (i < g.length && j < s.length) {
+            // 如果这块饼干的尺寸满足孩子的胃口，则分给他
             if (s[j] >= g[i]) {
-                res++;
+                // 遍历下一个孩子
+                i++;
+                // 遍历下一块饼干
+                j++;
+                // 满足数 +1
+                count++;
+            } else {
+                // 不满足孩子胃口，则饼干等一等，遍历下一个孩子
                 i++;
             }
-            j++;
         }
 
-        return res;
+        return count;
     }
 }
